a474ea7f5e9b249baba469cfe091015425f96128
Chun Zen Marc Tse <32941251+MarcieMarc425@users.noreply.github.com>
2018-07-12 01:13:09 +0800

    Updated battle.js

--- cocos/battle.js
+++ cocos/battle.js
@@ -36,6 +36,13 @@ cc.Class({
     // LIFE-CYCLE CALLBACKS:
 
     onLoad () {
+        var _this = this;
+        socket.emit('display-turn', 'loaded', playerTurn => {
+          _this.setTurn(playerTurn);
+        });
+        socket.on('display-turnPlus', res => {
+          _this.setTurn(res);
+        });
         this.p1Pos = [0, 0];
         this.player1Pos.node.position = cc.p(0, -99.5);
     },
@@ -45,6 +52,8 @@ cc.Class({
     },
     
     moveUpLeft () {
+        var _this = this;
+        socket.emit('end-turn', 'end');
         if((this.p1Pos[1] + 1) <= 4) {
             this.p1Pos[1] += 1;
             this.player1Pos.node.x -= 69.5;
@@ -53,6 +62,8 @@ cc.Class({
     },
 
     moveUpRight () {
+        var _this = this;
+        socket.emit('end-turn', 'end');
         if((this.p1Pos[0] + 1) <= 4) {
             this.p1Pos[0] += 1;
             this.player1Pos.node.x += 69.5;
@@ -61,6 +72,8 @@ cc.Class({
     },
 
     moveDownLeft () {
+        var _this = this;
+        socket.emit('end-turn', 'end');
         if((this.p1Pos[0] - 1) >= 0) {
             this.p1Pos[0] -= 1;
             this.player1Pos.node.x -= 69.5;
@@ -69,6 +82,8 @@ cc.Class({
     },
 
     moveDownRight () {
+        var _this = this;
+        socket.emit('end-turn', 'end');
         if((this.p1Pos[1] - 1) >= 0) {
             this.p1Pos[1] -= 1;
             this.player1Pos.node.x += 69.5;
@@ -76,6 +91,20 @@ cc.Class({
         }
     },
 
+    setTurn(playerTurn) {
+        if(playerTurn == '对方（红色）') {
+          this.up_left.interactable = false;
+          this.up_right.interactable = false;
+          this.down_left.interactable = false;
+          this.down_right.interactable = false;
+        } else {
+          this.up_left.interactable = true;
+          this.up_right.interactable = true;
+          this.down_left.interactable = true;
+          this.down_right.interactable = true;
+        }
+        this.turnNotification.string = '回合:' + playerTurn;
+      }
 
     // update (dt) {},
 });
