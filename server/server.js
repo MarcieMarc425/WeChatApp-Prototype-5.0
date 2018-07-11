e9a09cdc5a242357f8524cae0fa9a2194bc49cc1
Chun Zen Marc Tse <32941251+MarcieMarc425@users.noreply.github.com>
2018-07-12 01:14:09 +0800

    Updated server.js

--- server/server.js
+++ server/server.js
@@ -9,7 +9,10 @@ app.get('*', function (req, res) {});
 // Defined variables
 var connectedQueue = [];
 var pairingQueue = [];
-
+var room = [];
+var p1 = '';
+var p2 = '';
+var playerTurn = '';
 
 // When a player connects
 io.on('connect', socket => {
@@ -31,7 +34,7 @@ io.on('connect', socket => {
         if(found)
             return;
         
-            // Player is not in queue, adds to queue
+        // Player is not in queue, adds to queue
         else {
             pairingQueue.push(socket.id);
             console.log('Players currently connected: ' + connectedQueue.length);
@@ -45,8 +48,18 @@ io.on('connect', socket => {
             // Current waiting queue has 2 players, pair them
             else {
                 callback('paired');
+                // Set p1 & p2
+                for(var i = 0; i < pairingQueue.length; i++) {
+                    if(i == 0)
+                        p1 = pairingQueue[i];
+                    else
+                        p2 = pairingQueue[i];
+                }
+                // Set player turn to p1 (default)
+                playerTurn = p1;   
                 for(var i = 0; i < pairingQueue.length; i++) {
                     io.to(pairingQueue[i]).emit('pairing-success', 'ready');
+                    room.push(pairingQueue[i]);
                     pairingQueue.splice(i, 1);
                     i -= 1;
                 }
@@ -56,6 +69,26 @@ io.on('connect', socket => {
         }
     });
 
+    socket.on('display-turn', (res, callback) => {
+        var string = '';
+        if(socket.id == playerTurn)
+            string = '你（蓝色）';
+        else
+            string = '对方（红色）';
+        callback(string);
+    });
+
+    socket.on('end-turn', res => {
+        changeTurn();
+        var string = '';
+        for(var i = 0; i < room.length; i++) {
+            if(room[i] == playerTurn) {
+                io.to(room[i]).emit('display-turnPlus', '你（蓝色）');
+            } else {
+                io.to(room[i]).emit('display-turnPlus', '对方（红色）');
+            }
+        }
+    });
 
     // When client disconnects
     socket.on('disconnect', res => {
@@ -64,8 +97,14 @@ io.on('connect', socket => {
         console.log('Players currently connected: ' + connectedQueue.length);
         console.log('Players currently waiting in queue: ' + pairingQueue.length)
     });
-
 });
+
+function changeTurn() {
+    if(playerTurn == p1)
+        playerTurn = p2;
+    else
+        playerTurn = p1;
+}
 
 http.listen(PORT, () => {
     console.log('Server listening on port ' + PORT + '...');
