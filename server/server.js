a7cdfc9c0affbd1d9fb2ad3e045178fbe856ef62
marctse <marctse@tencent.com>
2018-07-13 17:30:53 +0800

    Updated server.js

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ io.on('connect', socket => {
         
         // If same player is already in queue, button has no effect
         for(var i = 0; i < pairingQueue.length; i++) {
-            if(pairingQueue[i] == socket.id)
+            if(pairingQueue[i].id == socket.id)
                 found = true;
         }
         if(found)
@@ -38,7 +38,10 @@ io.on('connect', socket => {
         
         // Player is not in queue, adds to queue
         else {
-            pairingQueue.push(socket.id);
+            var playerObj = new Object();
+            playerObj.id = socket.id;
+            playerObj.hero = res;
+            pairingQueue.push(playerObj);
             console.log('Players currently connected: ' + connectedQueue.length);
             console.log('Players currently waiting in queue: ' + pairingQueue.length)
             
@@ -58,14 +61,14 @@ io.on('connect', socket => {
                         p2 = pairingQueue[i];
                 }
                 // Set player turn to p1 (default)
-                playerTurn = p1;
+                // playerTurn = p1;
                 
                 // Set players initial coordinates
                 p1Pos = [0, 0];
                 p2Pos = [4, 4];
 
                 for(var i = 0; i < pairingQueue.length; i++) {
-                    io.to(pairingQueue[i]).emit('pairing-success', 'ready');
+                    io.to(pairingQueue[i].id).emit('pairing-success', 'ready');
                     room.push(pairingQueue[i]);
                     pairingQueue.splice(i, 1);
                     i -= 1;
@@ -76,54 +79,71 @@ io.on('connect', socket => {
         }
     });
 
-    // Display players' turn for client (On load only)
-    socket.on('display-turn', (res, callback) => {
-        var string = '';
-        if(socket.id == playerTurn)
-            string = '你（蓝色）';
-        else
-            string = '对方（红色）';
-        callback(string);
-    });
+    // // Display players' turn for client (On load only)
+    // socket.on('display-turn', (res, callback) => {
+    //     var string = '';
+    //     if(socket.id == playerTurn)
+    //         string = '你（蓝色）';
+    //     else
+    //         string = '对方（红色）';
+    //     callback(string);
+    // });
 
-    // Player has selected an action and end his turn
-    socket.on('end-turn', res => {
+    // // Player has selected an action and end his turn
+    // socket.on('end-turn', res => {
 
-        // If the player ending turn == p1, no need to invert action on server coord
-        if(socket.id == p1)
-            movePlayerPos(p1, res);
-        else
-            movePlayerPos(p2, invertPlayerAction(res));
+    //     // If the player ending turn == p1, no need to invert action on server coord
+    //     if(socket.id == p1.id)
+    //         movePlayerPos(p1.id, res);
+    //     else
+    //         movePlayerPos(p2.id, invertPlayerAction(res));
 
-        // Broadcast move action to the player NOT ending the turn, since client already registers own action
-        var otherPlayer = '';
-        // If p1 == other player, register p2's action on p1 client
-        // Since p2 server coord is already in sync with p1 client's view of p2's action, no need to invert
-        if(socket.id != p1) 
-            io.to(p1).emit('display-otherPlayerPos', p2Pos);
-        else
-            io.to(p2).emit('display-otherPlayerPos', invertPlayerPosView(p1Pos));
+    //     // Broadcast move action to the player NOT ending the turn, since client already registers own action
+    //     var otherPlayer = '';
+    //     // If p1 == other player, register p2's action on p1 client
+    //     // Since p2 server coord is already in sync with p1 client's view of p2's action, no need to invert
+    //     if(socket.id != p1.id) 
+    //         io.to(p1.id).emit('display-otherPlayerPos', p2Pos);
+    //     else
+    //         io.to(p2.id).emit('display-otherPlayerPos', invertPlayerPosView(p1Pos));
 
-        // Change the players' turn
-        changeTurn();
-        var string = '';
+    //     // Change the players' turn
+    //     changeTurn();
+    //     var string = '';
 
-        // Emit the changed turn effect to players in room
-        for(var i = 0; i < room.length; i++) {
-            if(room[i] == playerTurn) {
-                io.to(room[i]).emit('display-turnPlus', '你（蓝色）');
-            } else {
-                io.to(room[i]).emit('display-turnPlus', '对方（红色）');
-            }
-        }
+    //     // Emit the changed turn effect to players in room
+    //     for(var i = 0; i < room.length; i++) {
+    //         if(room[i] == playerTurn) {
+    //             io.to(room[i]).emit('display-turnPlus', '你（蓝色）');
+    //         } else {
+    //             io.to(room[i]).emit('display-turnPlus', '对方（红色）');
+    //         }
+    //     }
 
         
-    });
+    // });
+
+    socket.on('cancel-pairing', (header, res) => {
+        for(var i = 0; i < pairingQueue.length; i++) {
+            if(pairingQueue[i].id == socket.id)
+                pairingQueue.splice(i, 1);
+        }
+        console.log('Players currently connected: ' + connectedQueue.length);
+        console.log('Players currently waiting in queue: ' + pairingQueue.length)
+        res('success');
+    })
 
     // When client disconnects
     socket.on('disconnect', res => {
         console.log('A user has disconnected.');
-        connectedQueue.splice(socket.id, 1);
+        for(var i = 0; i < connectedQueue.length; i++) {
+            if(connectedQueue[i] == socket.id)
+                connectedQueue.splice(i, 1);
+        }
+        for(var i = 0; i < pairingQueue.length; i++) {
+            if(pairingQueue[i] == socket.id)
+                pairingQueue.splice(i, 1);
+        }
         console.log('Players currently connected: ' + connectedQueue.length);
         console.log('Players currently waiting in queue: ' + pairingQueue.length)
     });
@@ -148,39 +168,39 @@ function invertPlayerAction(action) {
         return 'moveUpLeft';
 }
 
-// Register move action on players' server coordinates
-function movePlayerPos(player, action) {
-    if(player == p1) {
-        // Change player coord according to action
-        if(action == 'moveUpRight')
-            p1Pos = [p1Pos[0]+1, p1Pos[1]];
-        else if(action == 'moveUpLeft')
-            p1Pos = [p1Pos[0], p1Pos[1]+1];
-        else if(action == 'moveDownLeft')
-            p1Pos = [p1Pos[0]-1, p1Pos[1]];
-        else if(action == 'moveDownRight')
-            p1Pos = [p1Pos[0], p1Pos[1]-1];
-    } else {
-        if(action == 'moveUpRight')
-            p2Pos = [p2Pos[0]+1, p2Pos[1]];
-        else if(action == 'moveUpLeft')
-            p2Pos = [p2Pos[0], p2Pos[1]+1];
-        else if(action == 'moveDownLeft')
-            p2Pos = [p2Pos[0]-1, p2Pos[1]];
-        else if(action == 'moveDownRight')
-            p2Pos = [p2Pos[0], p2Pos[1]-1];
-    }
-    console.log(p1Pos);
-    console.log(p2Pos);
-}
+// // Register move action on players' server coordinates
+// function movePlayerPos(player, action) {
+//     if(player == p1) {
+//         // Change player coord according to action
+//         if(action == 'moveUpRight')
+//             p1Pos = [p1Pos[0]+1, p1Pos[1]];
+//         else if(action == 'moveUpLeft')
+//             p1Pos = [p1Pos[0], p1Pos[1]+1];
+//         else if(action == 'moveDownLeft')
+//             p1Pos = [p1Pos[0]-1, p1Pos[1]];
+//         else if(action == 'moveDownRight')
+//             p1Pos = [p1Pos[0], p1Pos[1]-1];
+//     } else {
+//         if(action == 'moveUpRight')
+//             p2Pos = [p2Pos[0]+1, p2Pos[1]];
+//         else if(action == 'moveUpLeft')
+//             p2Pos = [p2Pos[0], p2Pos[1]+1];
+//         else if(action == 'moveDownLeft')
+//             p2Pos = [p2Pos[0]-1, p2Pos[1]];
+//         else if(action == 'moveDownRight')
+//             p2Pos = [p2Pos[0], p2Pos[1]-1];
+//     }
+//     console.log(p1Pos);
+//     console.log(p2Pos);
+// }
 
-// Function for changing players' turns
-function changeTurn() {
-    if(playerTurn == p1)
-        playerTurn = p2;
-    else
-        playerTurn = p1;
-}
+// // Function for changing players' turns
+// function changeTurn() {
+//     if(playerTurn == p1)
+//         playerTurn = p2;
+//     else
+//         playerTurn = p1;
+// }
 
 http.listen(PORT, () => {
     console.log('Server listening on port ' + PORT + '...');
